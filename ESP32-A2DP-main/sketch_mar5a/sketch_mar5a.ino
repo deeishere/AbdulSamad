d87edcab3e279ba1b6b560d23972aee16d65e000
dee <dee0gh@outlook.com>
2025-03-06 13:59:05 +0300

    Sound

---
+++ ESP32-A2DP-main/sketch_mar5a/sketch_mar5a.ino
@@ -0,0 +1,12 @@
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+
+AnalogAudioStream out;
+BluetoothA2DPSink a2dp_sink(out);
+
+void setup() {
+    a2dp_sink.start("MyMusic");
+}
+
+void loop() {
+}
